d6244061faef2daff64e4ac1f6130958e6af662e
daviderose526 <daviderose526@gmail.com>
2018-06-20 14:49:54 -0500

    Added routing with Get and Post requests to HelloController.

--- HelloMVC/Controllers/HelloController.cs
+++ HelloMVC/Controllers/HelloController.cs
@@ -11,9 +11,30 @@ namespace HelloMVC.Controllers
     public class HelloController : Controller
     {
         // GET: /<controller>/
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Index(string name)
         {
-            return Content("<h1>Hello World</h1>", "text/html");
+            string html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet me!' />" +
+                "</form>";
+
+            return Redirect("/Hello/Goodbye");
+        }
+
+        // /Hello
+        [Route("/Hello")]
+        [HttpPost]
+        public IActionResult Display(string name = "World")
+        {
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
+        }
+
+        // Handle requests to /Hello/NAME (URL segment)
+        [Route("/Hello/{name}")]
+        public IActionResult Index2(string name)
+        {
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
         }
 
         public IActionResult Goodbye()
